0152bc47935a19ada2703987a957c0c960622637
xiaoxiaoqingyi <lideyi333@163.com>
2017-06-29 19:03:44 +0800

    variable  assignment 变量赋值

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 世界")
+
+	//第一种赋值方式
+	var a int
+	a = 1
+
+	//第二种赋值方式
+	var b = 2
+
+	//第三种赋值方式
+	c := 10
+
+	var d, e int = 1, 2
+	var f, g = 123, "hello"
+
+	//这种不带声明格式的只能在函数体中出现
+	h, i := 123, "hello"
+
+	fmt.Println("Hello, 世界", a, b, c, d, e, f, g, h, i)
 }
